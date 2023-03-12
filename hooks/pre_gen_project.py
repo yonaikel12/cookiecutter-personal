97220029288a3b87305b5608244da2bdedfe1f66
yonaikel12 <103503023+yonaikel12@users.noreply.github.com>
2023-03-11 23:04:01 -0400

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -13,7 +13,7 @@ project_slug = "{{ cookiecutter.project_slug }}"
 
 ERROR_COLOR = "\x1b[31m"
 MESSAGE_COLOR = "\x1b[34m"
-RESET_ALL = "\xib[0m"
+RESET_ALL = "\x1b[0m"
 
 if project_slug.startswith("x"):
     print(f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template.{RESET_ALL}")
