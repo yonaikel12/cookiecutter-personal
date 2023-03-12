1c80d25857b9b117a8c145adabb89ae99579f6c6
yonaikel12 <103503023+yonaikel12@users.noreply.github.com>
2023-03-11 23:06:29 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,7 @@ print("bye world")
 import subprocess
 
 MESSAGE_COLOR = "\x1b[34m"
-RESET_ALL = "\xib[0m"
+RESET_ALL = "\x1b[0m"
 
 print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
